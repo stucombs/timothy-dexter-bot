8d00b209e0f13d003b3f4bb2d1714a897c5efde4
stucombs <stu.combs@icloud.com>
2022-06-09 08:10:25 -0400

    Add readfile parameter, remove bearer token

--- classes/Tweeter.class.php
+++ classes/Tweeter.class.php
@@ -2,7 +2,7 @@
 /*
 * FILE: classes/Tweeter.class.php
 * DATE: 01/08/2022
-* DESCRIPTION:
+* DESCRIPTION: class for authentication and posting tweet
 * AUTHOR: stucombs at icloud dot com
 */
 
@@ -11,14 +11,17 @@ class Tweeter {
     protected $oauth_access_token_secret;
     protected $consumer_key;
     protected $consumer_secret;
-    protected $bearer;
     protected $url;
-    protected $read_file = '../apickle.txt';
+    protected $read_file;
 
-    public function __construct($settings){
+    /**
+     * @param array $settings array of config settings for account
+     * @param string $read_file path to readable file
+    */
+    public function __construct($settings, $read_file){
         $this->url                          = 'https://api.twitter.com/1.1/statuses/update.json';
+        $this->read_file                    = $read_file;
         $this->account_id                   = $settings['ACCOUNT_ID'];
-        $this->bearer                       = $settings['BEARER_TOKEN'];
         $this->oauth_access_token           = $settings['ACCESS_TOKEN'];
         $this->oauth_access_token_secret    = $settings['ACCESS_TOKEN_SECRET'];
         $this->consumer_key                 = $settings['CONSUMER_KEY'];
@@ -26,9 +29,9 @@ class Tweeter {
     }
 
     /**
-        @param $tweet string a string in which to post as a 'tweet'
-
-        @return string (Json object)
+     * Authenticates and posts a $tweet
+     * @param string $tweet a character string in which to post as a 'tweet'
+     * @return string (Json object)
     */
     public function postTweet($tweet){
         $oauth_nonce = $this->createNonce();
@@ -55,6 +58,13 @@ class Tweeter {
         return $results;
     }
 
+    /**
+     * Create authentication header with OAuth1.0a standards 
+     * @param string $nonce unique token generated for each request
+     * @param string $timestamp unique timestamp for authentication
+     * @param string $signature genereated authentication signature from $this->createSignature
+     * @return string $oauthheader
+    */
     public function createAuthHeader($nonce, $timestamp, $signature){
         $oauth_header = '';
         $oauth_header .= 'OAuth oauth_consumer_key="' . rawurlencode($this->consumer_key) . '",';
@@ -63,11 +73,18 @@ class Tweeter {
         $oauth_header .= 'oauth_signature_method="HMAC-SHA1",';
         $oauth_header .= 'oauth_timestamp="' . rawurlencode($timestamp) . '",';
         $oauth_header .= 'oauth_token="' . rawurlencode($this->oauth_access_token) . '",';
-        $oauth_header .= 'oauth_version="1.0",';
+        $oauth_header .= 'oauth_version="1.0"';
 
         return $oauth_header;
     }
 
+    /**
+     * Create authentication signature to Twitter APIv2 standards 
+     * @param string $nonce unique token generated for each request
+     * @param string $timestamp timestamp for authentication
+     * @param string $tweet string to be posted as a 'tweet'
+     * @return string $signature
+    */
     public function createSignature($nonce, $timestamp, $tweet){
         $oauth_hash = '';
         $oauth_hash .= 'oauth_consumer_key=' . $this->consumer_key;
@@ -85,16 +102,27 @@ class Tweeter {
         return $signature;
     }
 
+    /**
+     * Generate a unique token for each request
+     * @return string $nonce
+    */
     public function createNonce(){
         $nonce = base64_encode(uniqid());
         $nonce = preg_replace('~[\W]~','',$nonce);
         return $nonce;
     }
 
+    /**
+     * Retrieve random line from readfile contents
+     * @return string $line
+    */
     public function getTweetText(){
-        $f_contents = file('apickle.txt');
-        $line = $f_contents[array_rand($f_contents)];
-        return $line . "\n";
+        $f_contents = file($this->read_file);
+        $line = '';
+        while( $line === '' ){
+            $line = $f_contents[array_rand($f_contents)];
+        }
+        return $line;
     }
 }
 
